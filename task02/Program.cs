de187a927af619dd8dd0acc4bb8ff371bf024749
okonoki <okonokiss@gmail.com>
2023-07-21 13:57:36 +0500

    task02 is done

--- task02/Program.cs
+++ task02/Program.cs
@@ -3,3 +3,26 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int N = Prompt("Введите число: ");
+
+void SumFigure(int N)
+{
+    int n = 0;
+    int sum = 0;
+    while (N > 0)
+    {
+        n = N % 10;
+        sum = sum + n;
+        N = N / 10;
+    }
+    Console.Write(sum);
+}
+
+SumFigure(N);
