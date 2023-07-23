3405236ee3c7317ac6c1c6086dd4f8f65a594f3c
okonoki <okonokiss@gmail.com>
2023-07-23 17:03:52 +0500

    task03_2 is done

---
+++ task03_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+int size = Prompt("Введите длину массива: ");
+int min = Prompt("Введите начальное значение диапазона: ");
+int max = Prompt("Введите конечное значение диапазона: ");
+int[] array = GenerateArray(size, min, max);
+PrintArray(array);
